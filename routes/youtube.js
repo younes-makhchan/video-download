7307a27ad77c07c866aca328ead3fc7ed6597002
Younes Makhchan <younessmakhchane@gmail.com>
2023-02-21 23:57:43 +0100

    debug

--- routes/youtube.js
+++ routes/youtube.js
@@ -4,10 +4,23 @@ const axios=require("axios")
 
 
 router.get("/",(req,res,next)=>{
-    res.render("youtube/youtube");
+    
+    res.render("youtube/youtube",{
+        oldInput:null,
+        error:null
+    });
 })
-router.post("/mp3",async (req,res,next)=>{
-    let musicName=req.body.musicName+" lyrics";
+router.post("/",async (req,res,next)=>{
+    let musicName=req.body.musicName;
+    if(musicName.indexOf("http")>-1 ||musicName.indexOf(".com")>-1||musicName.indexOf("wwww.youtube")>-1){
+        res.render("youtube/youtube",{
+            oldInput:{musicName:musicName},
+            error:"bad music name"
+        });
+    }
+     musicName+=" lyrics";
+
+
     const response=await axios.get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q="+musicName+"&key=AIzaSyBFYvy18_rgbpcfoJNpNLvWzdtXMnlPK50");
     const videoId=response.data.items[0].id.videoId;
     // const {url}=req.body;
@@ -22,7 +35,9 @@ router.post("/mp3",async (req,res,next)=>{
         }
       };
       axios.request(options).then(function (response) {
-          res.redirect(response.data.link);
+
+            res.redirect(response.data.link);
+          
       }).catch(function (error) {
           console.error(error);
       });
