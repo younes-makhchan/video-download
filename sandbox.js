08742716b35643e5bcd73c7164a646e38ec2ac74
Younes Makhchan <younessmakhchane@gmail.com>
2023-02-22 13:29:01 +0100

    update the note

--- sandbox.js
+++ sandbox.js
@@ -1,54 +1,17 @@
+const axios = require("axios");
 
-var fs = require('fs');
-var request = require('request');
+const options = {
+  method: 'GET',
+  url: 'https://youtube-mp37.p.rapidapi.com/get',
+  params: {id: 'MwpMEbgC7DA'},
+  headers: {
+    'X-RapidAPI-Key': '7df89c8ba9msh8d6ee421539a58ep18d983jsn13a652c76243',
+    'X-RapidAPI-Host': 'youtube-mp37.p.rapidapi.com'
+  }
+};
 
-function downloadFile(file_url, targetPath, cb) {
-    var received_bytes = 0;
-    var total_bytes = 0;
-    var  beginData  =  new  Date(). getTime()
-
-    var req = request({
-        method: 'GET',
-        uri:file_url
-    });
-
-    var  out  =  fs. createWriteStream(`./music/${targetPath. replace(new  RegExp(  '/'  ,  "g"  ),'')}.mp3`);
-    req. pipe(out);
-
-    req. on('response',  data => {
-        total_bytes  =  parseInt(data. headers['content-length']);
-    });
-
-    req. on('data',  chunk => {
-        received_bytes  +=  chunk. length;
-    });
-
-    req. on('end', _ => {
-        var  endData  =  new  Date(). getTime()
-        var elaTime = (endData - beginData) / 1000
-        console. log("\n\n下载成功 | 耗时: "  +  elaTime  +  "s",  (total_bytes  /  1000000). toFixed(1)  +  'm |' ,  targetPath  +  '.mp3');
-        cb()
-    });
-}
-
-
-
-var i = 0;
-
-function recursion() {
-    var  name  =  data[i]. ar[0]. name
-    if  (data[i]. ar. length  >  1)
-        data[i]. ar. forEach((item,  index) => {
-            if (index != 0)
-                name  =  name  +  ','  +  item. name
-        });
-    name  =  name  +  ' - '  +  data[i]. name
-
-    downloadFile("https://music.163.com/song/media/outer/url?id="  +  data[i]. id  +  '.mp3',  name, () => {
-        i++
-        if  (i  <=  data. length)
-            recursion()
-    })
-}
-recursion()
-downloadFile("https://music.163.com/song/media/outer/url?id=386538.mp3", 'test', _ => {}) //test
+axios.request(options).then(function (response) {
+	console.log(response.data.file);
+}).catch(function (error) {
+	console.error(error);
+});
